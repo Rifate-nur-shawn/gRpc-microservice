dbff50103e26128a5fedd5e48645eed29b5eb176
shawn <rnshawn99@gmail.com>
2025-12-22 16:57:45 +0600

    feat: implement Add RPC in Calculate service with related server setup

--- main.go
+++ main.go
@@ -1,27 +1,40 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net"
 
+	pb "github.com/rifate-nur-shawn/gRpc-microservice/pb/proto"
 	"google.golang.org/grpc"
 )
 
+type server struct {
+	pb.UnimplementedCalculateServer
+}
+
+func (s *server) Add(ctx context.Context, req *pb.AddRequest) (*pb.AddResponce, error) {
+	return &pb.AddResponce{
+		Sum: req.A + req.B,
+	}, nil
+}
+
 func main() {
-    port := ":50051"
-    lis , err := net.Listen("tcp",port )
-    if err!= nil{
-        log.Fatal("Faield to listen",err)
-    }
-    grpcServer := grpc.NewServer()
+	port := ":50051"
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatal("Failed to listen ", err)
 
+	}
+	grpcServer := grpc.NewServer()
 
-//skiping something here
+	pb.RegisterCalculateServer(grpcServer,&server{})
 
-log.Println("service is running on port", port)
+	log.Println("server running on port :", port)
 
- err = grpcServer.Serve(lis)
- if err!= nil{
-    log.Fatal("Faield to serve",err)
- }
+	err = grpcServer.Serve(lis)
+	if err != nil {
+		log.Fatal("Failed to serve ", err)
+	}
+
 }
