5e02ae8f6d279133b522b41f165347024a823bf8
shawn <rnshawn99@gmail.com>
2025-12-22 15:05:27 +0600

    feat: implement Calculate service with Add RPC and related protobuf definitions

--- main.go
+++ main.go
@@ -1,45 +1,27 @@
 package main
 
 import (
-    "context"
-    "log"
-    pb "github.com/rifate-nur-shawn/gRpc-microservice/pb"
-    "google.golang.org/grpc"
-    "google.golang.org/grpc/credentials/insecure"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
-    // Step 4.1: Connect to the server
-    conn, err := grpc.Dial("localhost:50051", 
-        grpc.WithTransportCredentials(insecure.NewCredentials()))
-    if err != nil {
-        log.Fatalf("Failed to connect: %v", err)
+    port := ":50051"
+    lis , err := net.Listen("tcp",port )
+    if err!= nil{
+        log.Fatal("Faield to listen",err)
     }
-    defer conn.Close()
+    grpcServer := grpc.NewServer()
 
-    // Step 4.2: Create client using the GENERATED function
-    client := pb.NewProductServiceClient(conn)
 
-    // Step 4.3: Call CreateProduct (send a request)
-    createResp, err := client.CreateProduct(context.Background(), &pb.CreateProductRequest{
-        Product: &pb.Product{
-            Id:       "123",
-            Name:     "Gaming Laptop",
-            Price:    2500.00,
-            Category: "Electronics",
-        },
-    })
-    if err != nil {
-        log.Fatalf("Error creating product: %v", err)
-    }
-    log.Printf("Product created with ID: %s\n", createResp.Id)
+//skiping something here
 
-    // Step 4.4: Call GetProduct (send another request)
-    getResp, err := client.GetProduct(context.Background(), &pb.GetProductRequest{
-        Id: "123",
-    })
-    if err != nil {
-        log.Fatalf("Error getting product: %v", err)
-    }
-    log.Printf("Product: %s, Price: $%.2f\n", getResp.Product.Name, getResp.Product.Price)
+log.Println("service is running on port", port)
+
+ err = grpcServer.Serve(lis)
+ if err!= nil{
+    log.Fatal("Faield to serve",err)
+ }
 }
